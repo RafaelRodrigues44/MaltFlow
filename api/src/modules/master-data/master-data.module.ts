bc3a9aa6dd4f2792f94fd11355b2542c0c4f96c1
RafaelRodrigues44 <rafael.rodrigues85@hotmail.com>
2026-03-06 11:38:05 -0300

    fix: modify master-data-module.ts

--- api/src/modules/master-data/master-data.module.ts
+++ api/src/modules/master-data/master-data.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SA1Entity } from './entities/sa1.entity';
+import { SA2Entity } from './entities/sa2.entity';
+import { SB1Entity } from './entities/sb1.entity';
 import { MasterDataService } from './master-data.service';
 import { MasterDataController } from './master-data.controller';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([SA1Entity, SA2Entity, SB1Entity]),
+  ],
   controllers: [MasterDataController],
   providers: [MasterDataService],
+  exports: [MasterDataService],
 })
 export class MasterDataModule {}
