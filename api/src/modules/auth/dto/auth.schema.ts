248ae93f2cfd799528aece7fa0400dc97aa9f4cf
RafaelRodrigues44 <rafael.rodrigues85@hotmail.com>
2026-03-06 16:22:06 -0300

    feature: create jwt an auth nodules

--- api/src/modules/auth/dto/auth.schema.ts
+++ api/src/modules/auth/dto/auth.schema.ts
@@ -2,12 +2,12 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty({ example: 'admin', description: 'O nome de usuário cadastrado na tabela ZUR' })
+  @ApiProperty({ example: 'user name', description: 'O nome de usuário cadastrado na tabela ZUR' })
   @IsNotEmpty()
   @IsString()
   username: string;
 
-  @ApiProperty({ example: 'barley123', description: 'A senha do colaborador' })
+  @ApiProperty({ example: 'password', description: 'A senha do colaborador' })
   @IsNotEmpty()
   @IsString()
   password: string;
